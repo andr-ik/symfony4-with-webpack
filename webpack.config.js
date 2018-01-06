4cf874f6f004751b84e662a0aa8ea3260100dcd9
Andrey Mikhailov <andrey@roxot.com>
2018-01-06 06:30:23 +0300

    Add version.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,21 @@ require('dotenv').config();
 const env = process.env.APP_ENV || 'prod';
 const isDev = env !== 'prod';
 
+const fs = require('fs');
 const webpack = require('webpack');
 
 module.exports = {
     context: __dirname + '/assets/js',
     entry: {
+        common: './common',
         index: './index',
         contact: './contact'
     },
     output: {
         path: __dirname + '/public/assets/js/',
-        filename: './[name].js?v=[hash]'
+        publicPath: "/assets/",
+        filename: '[name].js',
+        //library: "[name]"
     },
     watch: isDev,
     devtool: isDev ? 'source-map' : false,
@@ -28,12 +32,24 @@ module.exports = {
                     presets: ['babel-preset-env']
                 }
             }
+        }, {
+            test: /\.(png|jpg|svg|ttf|eot|woff|woff2)/,
+            loader: 'file?name=[path][name].[ext]'
         }]
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
             name: "common"
-        })
+        }),
+        {
+            apply: function (compiler) {
+                compiler.plugin('done', function (stats) {
+                    fs.writeFileSync('./config/version.json', JSON.stringify({
+                        version: stats.hash
+                    }));
+                });
+            }
+        }
     ]
 };
 
